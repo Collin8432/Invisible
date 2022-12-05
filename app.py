4c86a15aed980fba071e09362d5298ee48fcc8e7
Astraltop <collinmc1228@gmail.com>
2022-12-05 16:47:53 -0500

    Fixed?

--- app.py
+++ app.py
@@ -16,6 +16,29 @@ def loadcogs():
    bot.load_extension(f"discordbot.cogs.__init__")
    bot.load_extension(f"discordbot.cogs.listeners.__init__")
    
+
+
+
+def create_app(test_config=True):
+    # create and configure the app
+    app = Flask(__name__,instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY='dev')
+
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
          
 app = Flask(__name__)
 
@@ -97,7 +120,7 @@ def adminpage():
 if __name__ == "__main__":
    loadcogs()
    print("Starting up {} shard{}...".format(bot.shard_count,"" if bot.shard_count == 1 else "s"))
-   partial_run = partial(app.run, host="0.0.0.0", port="$PORT", debug=True, use_reloader=False)
+   partial_run = partial(app.run, port=os.getenv("PORT"), debug=True, use_reloader=False)
    t = Thread(target=partial_run)
    t.start()
 
