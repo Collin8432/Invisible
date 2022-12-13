9d3e6879ffe3609c1a03410961631a05bb8acd3e
Collin <collinmc1228@gmail.com>
2022-12-13 13:44:21 -0500

    Dj Khaled

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: waitress-serve --listen=*:5000 incognito.wsgi:application
-worker: python bot.py
--- commands/management/commands/RunBot.py
+++ commands/management/commands/RunBot.py
@@ -1,3 +1,5 @@
+from django.core.management.base import BaseCommand
+
 import disnake
 from disnake.ext.commands import AutoShardedInteractionBot
 
@@ -11,8 +13,13 @@ load_dotenv(".env")
 def loadcogs():
    bot.load_extension(f"discordbot.cogs.__init__")
    bot.load_extension(f"discordbot.cogs.listeners.__init__")
-   
-if __name__ == "__main__":
-   loadcogs()
-   print("Starting up {} shard{}...".format(bot.shard_count,"" if bot.shard_count == 1 else "s"))
-   bot.run(os.environ.get("DISCORDTOKEN"))
+
+class Command(BaseCommand):
+    help = "Run a discord bot"
+
+    def handle(self, *args, **options):
+         loadcogs()
+         print("Starting up {} shard{}...".format(bot.shard_count,"" if bot.shard_count == 1 else "s"))
+         bot.run(os.environ.get("DISCORDTOKEN"))
+
+
